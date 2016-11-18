c053c8284f2a12290b0001719e56b2f3d2064079
jzhu063-macpro <zhujing0866@126.com>
2016-11-19 00:34:45 +0800

    jquery-weui

---
+++ WebAppBase/Pasys.Web.WeiXin.UI/Controllers/DemoController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pasys.Web.WeiXin.UI.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult actionsheet()
+        {
+            return View();
+        }
+
+        public ActionResult article()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        public ActionResult buttons()
+        {
+            return View();
+        }
+
+        public ActionResult cell()
+        {
+            return View();
+        }
+
+        public ActionResult form()
+        {
+            return View();
+        }
+
+        public ActionResult toast()
+        {
+            return View();
+        }
+
+        public ActionResult dialog()
+        {
+            return View();
+        }
+
+        public ActionResult progress()
+        {
+            return View();
+        }
+
+        public ActionResult msg()
+        {
+            return View();
+        }
+
+        public ActionResult icons()
+        {
+            return View();
+        }
+
+        public ActionResult panel()
+        {
+            return View();
+        }
+
+        public ActionResult tabbar()
+        {
+            return View();
+        }
+
+        public ActionResult searchbar()
+        {
+            return View();
+        }
+
+        public ActionResult toptip()
+        {
+            return View();
+        }
+
+        public ActionResult ptr()
+        {
+            return View();
+        }
+
+        public ActionResult infinite()
+        {
+            return View();
+        }
+
+        public ActionResult cols()
+        {
+            return View();
+        }
+
+        public ActionResult picker()
+        {
+            return View();
+        }
+
+        public ActionResult calendar()
+        {
+            return View();
+        }
+
+        public ActionResult citypicker()
+        {
+            return View();
+        }
+
+        public ActionResult datetimepicker()
+        {
+            return View();
+        }
+
+        public ActionResult swiper()
+        {
+            return View();
+        }
+
+        public ActionResult noti()
+        {
+            return View();
+        }
+
+        public ActionResult select()
+        {
+            return View();
+        }
+
+        public ActionResult popup()
+        {
+            return View();
+        }
+
+        public ActionResult photobrowser()
+        {
+            return View();
+        }
+
+    }
+}
--- WebAppBase/Pasys.Web.WeiXin.UI/Global.asax
+++ WebAppBase/Pasys.Web.WeiXin.UI/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pasys.Web.WeiXin.UI.UI.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pasys.Web.WeiXin.UI.MvcApplication" Language="C#" %>
