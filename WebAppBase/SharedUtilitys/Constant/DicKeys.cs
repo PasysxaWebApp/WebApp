1e794e57cf4a7120ceedffec08f4d868a84b2d28
jzhu063 <jzhu063@pasysxa.com>
2016-11-28 13:25:52 +0900

    add dickeys

---
+++ WebAppBase/SharedUtilitys/Constant/DicKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pasys.Core.Constant
+{
+    public class DicKeys
+    {
+        public const string RecordStatus = "RecordStatus";
+    }
+}
